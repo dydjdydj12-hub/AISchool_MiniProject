22d11d1ebf075b5f14c5e880807b2057f8b908bb
dydjdydj12-hub <chan981001@gmail.com>
2025-07-22 17:18:28 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("승찬!");
+        System.out.println("승찬2");
     }
 }
